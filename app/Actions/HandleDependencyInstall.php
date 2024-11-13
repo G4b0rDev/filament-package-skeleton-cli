9118d6af630cc4b290525b97feb566bcbea82660
g4b0rdev <gabordesigns@gmail.com>
2024-11-13 20:54:43 +0100

    feat: Add action to handle dependency installation

---
+++ app/Actions/HandleDependencyInstall.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions;
+
+use App\DataTransferObjects\Package;
+use Illuminate\Process\Pool;
+use Illuminate\Support\Facades\Process;
+
+class HandleDependencyInstall
+{
+    public function __invoke(Package $package): void
+    {
+        $basePath = getcwd().'/'.$package->name;
+
+        Process::concurrently(function (Pool $pool) use ($basePath) {
+            $pool
+                ->path($basePath)
+                ->command('composer install');
+
+            $pool
+                ->path($basePath)
+                ->command('npm install');
+        });
+    }
+}
