5cf6db0431867b9b172f8918e9c3abcfbca8a1c1
g4b0rdev <gabordesigns@gmail.com>
2024-11-16 23:59:17 +0100

    feat: Add ProjectAlreadyExistsException for handling duplicate project creation

---
+++ app/Exceptions/ProjectAlreadyExistsException.php
@@ -0,0 +1,16 @@
+
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+
+class ProjectAlreadyExistsException extends Exception
+{
+    public static function create(string $projectName): self
+    {
+        return new self("The project {$projectName} already exists.");
+    }
+}
