e65b4b989fdbfb9b6604af5fc33d994d81bcb988
g4b0rdev <gabordesigns@gmail.com>
2024-11-16 22:12:54 +0100

    chore: Add docblock and update transform keys

--- app/DataTransferObjects/Package.php
+++ app/DataTransferObjects/Package.php
@@ -4,17 +4,27 @@ declare(strict_types=1);
 
 namespace App\DataTransferObjects;
 
-use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Str;
 
-readonly class Package implements Arrayable
+/**
+ * @property string $name
+ * @property string $vendor
+ * @property FilamentPlugin $filamentPlugin
+ * @property Asset $asset
+ * @property string $testingSetup
+ * @property array $codeStyle
+ *
+ * @method static Package from(array $data): self
+ */
+readonly class Package
 {
     public function __construct(
         public string $name,
         public string $vendor,
-        public bool $standalone,
-        public ?Asset $asset,
+        public FilamentPlugin $filamentPlugin,
+        public Asset $asset,
         public string $testingSetup,
-        public array $linters = [],
+        public array $codeStyle = [],
     ) {
         //
     }
@@ -22,23 +32,12 @@ readonly class Package implements Arrayable
     public static function from(array $data): self
     {
         return new self(
-            name: $data['package'],
-            vendor: $data['vendor'],
-            standalone: $data['standalone'] ?? false,
-            asset: ($data['assets']) ? Asset::from($data['assets'], $data['custom_assets']) : null,
+            name: Str::slug($data['package']),
+            vendor: Str::slug($data['vendor']),
+            filamentPlugin: FilamentPlugin::from($data['filament']),
+            asset: $data['assets'],
             testingSetup: $data['testing'] ?? 'pest',
-            linters: $data['linters'] ?? [],
+            codeStyle: $data['codeStyle'] ?? [],
         );
-    }
-
-    public function toArray(): array
-    {
-        return [
-            'name' => $this->name,
-            'vendor' => $this->vendor,
-            'standalone' => $this->standalone,
-            'testingSetup' => $this->testingSetup,
-            'linters' => $this->linters,
-        ];
     }
 }
