ec3a3931127f111fb2eff49211d7bedc34ad34a6
g4b0rdev <gabordesigns@gmail.com>
2024-11-18 22:05:08 +0100

    test: Set up fallback for prompts in TestCase setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Laravel\Prompts\Prompt;
 use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Prompt::fallbackWhen(true);
+    }
 }
