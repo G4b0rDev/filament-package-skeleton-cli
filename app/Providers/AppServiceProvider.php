2ad6a0df704e2234cc6b69d7fd0eeaa04ffd0641
g4b0rdev <gabordesigns@gmail.com>
2024-12-14 00:54:34 +0100

    chore: Resolve ConfigHandler instance with default config path

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\ConfigHandler;
 use Illuminate\Support\ServiceProvider;
 
 // @pest-ignore
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        //
+        $this->app->singleton(ConfigHandler::class, function () {
+            return new ConfigHandler(configPath: getenv('HOME').'/.config/filament-package-skeleton/config.json');
+        });
     }
 
     public function register(): void
