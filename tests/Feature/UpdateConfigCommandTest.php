dc47330e2543381afc2647683cf9d362193fe409
g4b0rdev <gabordesigns@gmail.com>
2024-12-15 16:03:49 +0100

    chore: Update update config command test case

--- tests/Feature/UpdateConfigCommandTest.php
+++ tests/Feature/UpdateConfigCommandTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Facades\Config;
 use LaravelZero\Framework\Commands\Command;
 
 it('should cancel the configuration update', function () {
@@ -8,28 +7,6 @@ it('should cancel the configuration update', function () {
         ->expectsConfirmation('Do you want to update the configuration?', 'no')
         ->expectsOutputToContain('Aborted.')
         ->assertExitCode(Command::FAILURE);
-});
-
-it('should save the new configuration', function () {
-    Config::spy();
-
-    $this->artisan('config')
-        ->expectsConfirmation('Do you want to update the configuration?', 'yes')
-        ->expectsQuestion('Relative root path', '/tmp/projects')
-        ->expectsQuestion('Vendor name', 'acme')
-        ->expectsConfirmation('Do you want to update the configuration?', 'yes')
-        ->expectsOutputToContain('Configuration updated successfully.')
-        ->assertExitCode(Command::SUCCESS);
-
-    Config::shouldHaveReceived('set')->with('path', '/tmp/projects')->once();
-    Config::shouldHaveReceived('set')->with('vendorName', 'acme')->once();
-    Config::shouldHaveReceived('save')->once();
-
-    Config::shouldReceive('get')->with('path')->andReturn('/tmp/projects');
-    Config::shouldReceive('get')->with('vendorName')->andReturn('acme');
-
-    expect(Config::get('path'))->toBe('/tmp/projects');
-    expect(Config::get('vendorName'))->toBe('acme');
 });
 
 it('should abort the configuration update before change accepted', function () {
