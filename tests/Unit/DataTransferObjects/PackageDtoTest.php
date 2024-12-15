4ca7d7cecb60c696cfcb64d3b50f923a76236250
g4b0rdev <gabordesigns@gmail.com>
2024-12-15 15:52:26 +0100

    chore: Update package dto unit test case

--- tests/Unit/DataTransferObjects/PackageDtoTest.php
+++ tests/Unit/DataTransferObjects/PackageDtoTest.php
@@ -7,10 +7,11 @@ it('creates a Package DTO with specified package details', function () {
     $data = [
         'package' => fake()->word(),
         'vendor' => fake()->word(),
+        'isStandalone' => fake()->boolean(),
         'filament' => [
             'pluginName' => fake()->word(),
         ],
-        'custom_assets' => fake()->boolean(),
+        'assets' => null,
     ];
 
     $package = Package::from($data);
@@ -21,5 +22,5 @@ it('creates a Package DTO with specified package details', function () {
     expect($package->filamentPlugin)->toBeInstanceOf(FilamentPlugin::class);
     expect($package->filamentPlugin->pluginName)->toBe($data['filament']['pluginName']);
     expect($package->asset)->toBeNull();
-    expect($package->withAssets)->toBe($data['custom_assets']);
+    expect($package->withAssets)->toBe(! is_null($package->asset));
 });
