2221879f5a56c5d96ba470d3523f45d4f9acd63e
g4b0rdev <gabordesigns@gmail.com>
2024-12-22 00:24:39 +0100

    chore: Pass base path to stub handlers

--- app/Actions/PublishStubs.php
+++ app/Actions/PublishStubs.php
@@ -10,14 +10,18 @@ use App\Actions\StubHandlers\PublishAssetsHandler;
 use App\Actions\StubHandlers\ServiceProviderStubHandler;
 use App\DataTransferObjects\Author;
 use App\DataTransferObjects\Package;
+use App\Facades\Config;
+use Illuminate\Support\Str;
 
 class PublishStubs
 {
     public function __invoke(Package $package, Author $author): void
     {
-        MetaStubHandler::make($package, $author)();
-        ServiceProviderStubHandler::make($package, $author)();
-        PublishAssetsHandler::make($package, $author)();
-        FilamentPanelBuilderStubHandler::make($package, $author)();
+        $basePath = Config::basePath().'/'.Str::slug($package->name);
+
+        MetaStubHandler::make($package, $author, $basePath)();
+        ServiceProviderStubHandler::make($package, $author, $basePath)();
+        PublishAssetsHandler::make($package, $author, $basePath)();
+        FilamentPanelBuilderStubHandler::make($package, $author, $basePath)();
     }
 }
