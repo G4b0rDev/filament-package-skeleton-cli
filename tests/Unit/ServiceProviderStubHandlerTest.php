1c054e4fb7d99163f98489ef3ad6227b6e1094bd
g4b0rdev <gabordesigns@gmail.com>
2024-12-22 00:23:16 +0100

    chore: Update publish package service provider handler test case

--- tests/Unit/ServiceProviderStubHandlerTest.php
+++ tests/Unit/ServiceProviderStubHandlerTest.php
@@ -1,70 +1,28 @@
 <?php
 
-use App\Actions\StubHandlers\ServiceProviderStubHandler;
-use App\DataTransferObjects\Asset;
+use App\Actions\PublishProjectAction;
+use App\Actions\PublishStubs;
 use App\DataTransferObjects\Author;
-use App\DataTransferObjects\FilamentPlugin;
 use App\DataTransferObjects\Package;
-use Binafy\LaravelStub\Facades\LaravelStub;
-use Binafy\LaravelStub\Providers\LaravelStubServiceProvider;
-use Illuminate\Filesystem\FilesystemServiceProvider;
-use Illuminate\Support\Facades\File;
+use App\Facades\Config;
 
-beforeEach(function () {
-    app()->register(LaravelStubServiceProvider::class);
-    app()->register(FilesystemServiceProvider::class);
+it('should generate the package service provider', function () {
+    Config::set('path', __DIR__.'/../Package');
 
-    $this->basePath = getcwd() . '/TestPackage';
+    $package = Package::from([
+        'package' => 'my-simple-plugin',
+        'vendor' => 'test-user',
+        'isStandalone' => false,
+        'assets' => null,
+    ]);
 
-    $this->author = Author::from([
-        'name' => 'Test Author',
+    $author = Author::from([
+        'name' => 'Test User',
         'email' => 'test@example.com',
     ]);
 
-    $this->package = new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => false,
-        ]),
-        withAssets: false,
-    );
-});
+    app(PublishProjectAction::class)($package->name);
+    app(PublishStubs::class)($package, $author);
 
-it('should publish the service provider', function () {
-    $providerPath = "{$this->basePath}/src/TestPackageServiceProvider.php";
-    $skeletonPath = "{$this->basePath}/src/SkeletonServiceProvider.stub";
-
-    File::shouldReceive('isFile')
-        ->with($skeletonPath)
-        ->once()
-        ->andReturnTrue();
-
-    File::shouldReceive('delete')
-        ->with($skeletonPath)
-        ->once()
-        ->andReturnTrue();
-
-    LaravelStub::partialMock()
-        ->shouldReceive('generate')
-        ->once()
-        ->andReturnTrue();
-
-    File::shouldReceive('exists')
-        ->with($providerPath)
-        ->andReturnTrue();
-
-    File::shouldReceive('exists')
-        ->with($skeletonPath)
-        ->andReturnFalse();
-
-    ServiceProviderStubHandler::make($this->package, $this->author)();
-
-    expect(File::exists($providerPath))->toBeTrue();
-    expect(File::exists($skeletonPath))->toBeFalse();
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/src/MySimplePluginServiceProvider.php'))->toBeTrue();
 });
