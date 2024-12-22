828063e947eb2818f3f80526b084a4db5b6a69cb
g4b0rdev <gabordesigns@gmail.com>
2024-12-22 21:27:14 +0100

    chore: Update test case

--- tests/Unit/PublishAssetsHandlerTest.php
+++ tests/Unit/PublishAssetsHandlerTest.php
@@ -1,403 +1,104 @@
 <?php
 
-use App\Actions\StubHandlers\PublishAssetsHandler;
-use App\DataTransferObjects\Asset;
-use App\DataTransferObjects\Author;
-use App\DataTransferObjects\FilamentPlugin;
-use App\DataTransferObjects\Package;
-use Binafy\LaravelStub\Facades\LaravelStub;
-use Binafy\LaravelStub\Providers\LaravelStubServiceProvider;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Filesystem\FilesystemServiceProvider;
+use App\ConfigHandler;
+use App\Facades\Config;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
 
 beforeEach(function () {
-    app()->register(LaravelStubServiceProvider::class);
-    app()->register(FilesystemServiceProvider::class);
-
-    File::spy();
-    LaravelStub::spy();
-
-    $this->basePath = getcwd() . '/TestPackage';
-
-    $this->author = Author::from([
-        'name' => 'Test Author',
-        'email' => 'test@example.com',
-    ]);
-
-    $this->package = new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => false,
-            'withViews' => false,
-        ]),
-        withAssets: false,
+    App::instance(
+        ConfigHandler::class,
+        new ConfigHandler(configPath: __DIR__.'/../Package/filament-package-skeleton/config.json')
     );
+
+    Config::set('path', __DIR__.'/../Package');
+
+    Arr::map(File::directories(__DIR__.'/../Package'), fn (string $directory) => File::deleteDirectory($directory));
+
+    $packages = [
+        [
+            'package' => 'my-simple-plugin',
+            'vendor' => 'test-user',
+            'isStandalone' => true,
+            'asset' => [
+                'withCss' => true,
+                'customCss' => ['cssName' => 'app'],
+                'withJs' => true,
+                'customJs' => ['jsName' => 'app'],
+                'withViews' => true,
+            ],
+        ],
+        [
+            'package' => 'my-simple-plugin2',
+            'vendor' => 'test-user',
+            'isStandalone' => true,
+            'asset' => [
+                'withCss' => false,
+                'withJs' => false,
+                'withViews' => false,
+            ],
+        ],
+    ];
+
+    $author = [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+    ];
+
+    foreach ($packages as $package) {
+        generatePackage($package, $author, $package['asset']);
+    }
 });
 
-afterEach(function () {
-    File::swap(new Filesystem);
+afterAll(function () {
+    File::deleteDirectory(__DIR__.'/../Package/my-simple-plugin');
 });
 
-it('should not publish tailwindcss config', function (Package $package) {
-    $tailwindPath = "{$this->basePath}/tailwindcss.config.js";
+it('should publish tailwindcss config and base css', function () {
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/tailwindcss.config.js'))->toBeTrue();
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/css'))->toBeTrue();
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/css/app.css'))->toBeTrue();
+});
 
-    File::shouldReceive('isFile')
-        ->with($tailwindPath)
-        ->andReturnTrue();
+it('should not publish tailwindcss config and base css if no assets are set', function () {
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin2/tailwindcss.config.js'))->toBeFalse();
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin2/resources/css'))->toBeFalse();
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin2/resources/css/app.css'))->toBeFalse();
+});
 
-    File::shouldReceive('exists')
-        ->with($tailwindPath)
-        ->andReturnFalse();
+it('should publish javascript skeleton', function () {
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/js'))->toBeTrue();
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/js/app.js'))->toBeTrue();
+});
 
-    File::shouldReceive('delete')
-        ->with($tailwindPath)
-        ->andReturnTrue();
+it('should set the default javascript file name as the package name if no custom name is set', function () {
+    $asset = [
+        'withCss' => true,
+        'withJs' => true,
+    ];
 
-    PublishAssetsHandler::make($package, $this->author)();
-    expect(File::exists($tailwindPath))->toBeFalse();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
+    $package = [
+        'package' => 'my-simple-plugin3',
+        'vendor' => 'test-user',
+        'isStandalone' => false,
+        'asset' => $asset,
+    ];
 
-it('should publish tailwindcss config', function (Package $package) {
-    $tailwindPath = "{$this->basePath}/tailwindcss.config.js";
+    $author = [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+    ];
 
-    File::shouldReceive('exists')
-        ->with($tailwindPath)
-        ->once()
-        ->andReturnTrue();
+    generatePackage($package, $author, $asset);
 
-    PublishAssetsHandler::make($package, $this->author)();
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin3/resources/js/my-simple-plugin3.js'))->toBeTrue();
+});
 
-    expect(File::exists($tailwindPath))->toBeTrue();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => true,
-            'withJs' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
+it('should publish the views skeleton', function () {
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/views'))->toBeTrue();
+});
 
-it('should not publish the css', function (Package $package) {
-    $cssPath = "{$this->basePath}/resources/css";
-
-    PublishAssetsHandler::make($package, $this->author)();
-
-    File::shouldReceive('isDirectory')
-        ->with($cssPath)
-        ->andReturnTrue();
-
-    File::shouldReceive('exists')
-        ->with($cssPath)
-        ->andReturnTrue();
-
-    File::shouldReceive('deleteDirectory')
-        ->with($cssPath)
-        ->andReturnTrue();
-
-    expect(File::exists($cssPath))->toBeTrue();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should publish and rename the css file', function (Package $package) {
-    PublishAssetsHandler::make($package, $this->author)();
-
-    $oldCssPath = "{$this->basePath}/resources/css/skeleton.css";
-    $cssPath = "{$this->basePath}/resources/css/TestPackage.css";
-
-    File::shouldReceive('move')
-        ->with($oldCssPath, $cssPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('exists')
-        ->with($cssPath)
-        ->andReturnTrue();
-
-    expect(File::exists($cssPath))->toBeTrue();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => true,
-            'withJs' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should publish and rename custom css file', function (Package &$package) {
-    PublishAssetsHandler::make($package, $this->author)();
-
-    $oldCssPath = "{$this->basePath}/resources/css/skeleton.css";
-    $cssPath = "{$this->basePath}/resources/css/app.css";
-
-    File::shouldReceive('move')
-        ->with($oldCssPath, $cssPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('exists')
-        ->with($cssPath)
-        ->andReturnTrue();
-
-    expect(File::exists($cssPath))->toBeTrue();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => true,
-            'customCss' => [
-                'cssName' => 'app',
-            ],
-            'withJs' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should not publish javascript', function (Package $package) {
-    $jsPath = "{$this->basePath}/resources/js";
-
-    File::shouldReceive('isDirectory')
-        ->with($jsPath)
-        ->andReturnTrue();
-
-    File::shouldReceive('exists')
-        ->with($jsPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('deleteDirectory')
-        ->with($jsPath)
-        ->andReturnTrue();
-
-    PublishAssetsHandler::make($package, $this->author)();
-    expect(File::exists($jsPath))->toBeFalse();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should publish javascript', function (Package $package) {
-    $oldJsPath = "{$this->basePath}/resources/css/skeleton.js";
-    $jsPath = "{$this->basePath}/resources/css/TestPackage.js";
-
-    File::shouldReceive('move')
-        ->with($oldJsPath, $jsPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('exists')
-        ->with($oldJsPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('exists')
-        ->with($jsPath)
-        ->andReturnTrue();
-
-    PublishAssetsHandler::make($package, $this->author)();
-    expect(File::exists($oldJsPath))->toBeFalse();
-    expect(File::exists($jsPath))->toBeTrue();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => true,
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should publish javascript with custom name', function (Package $package) {
-    $oldJsPath = "{$this->basePath}/resources/css/skeleton.js";
-    $jsPath = "{$this->basePath}/resources/css/app.js";
-
-    File::shouldReceive('move')
-        ->with($oldJsPath, $jsPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('exists')
-        ->with($oldJsPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('exists')
-        ->with($jsPath)
-        ->andReturnTrue();
-
-    PublishAssetsHandler::make($package, $this->author)();
-    expect(File::exists($oldJsPath))->toBeFalse();
-    expect(File::exists($jsPath))->toBeTrue();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => true,
-            'customJs' => [
-                'jsName' => 'app',
-            ],
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should not publish the views', function (Package $package) {
-    $viewsPath = "{$this->basePath}/resources/views";
-
-    File::shouldReceive('isDirectory')
-        ->with($viewsPath)
-        ->andReturnTrue();
-
-    File::shouldReceive('exists')
-        ->with($viewsPath)
-        ->andReturnFalse();
-
-    File::shouldReceive('deleteDirectory')
-        ->with($viewsPath)
-        ->andReturnTrue();
-
-    PublishAssetsHandler::make($package, $this->author)();
-    expect(File::exists($viewsPath))->toBeFalse();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => false,
-            'withViews' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should publish the views', function (Package $package) {
-    $viewsPath = "{$this->basePath}/resources/views";
-
-    File::shouldReceive('exists')
-        ->with($viewsPath)
-        ->andReturnTrue();
-
-    PublishAssetsHandler::make($package, $this->author)();
-    expect(File::exists($viewsPath))->toBeTrue();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => false,
-            'withJs' => false,
-            'withViews' => true,
-        ]),
-        withAssets: true,
-    ),
-]);
-
-it('should should publish vite config', function () {
-    $vitePath = "{$this->basePath}/vite.config.js";
-    $viteStubPath = __DIR__ . '/../../stubs/vite.config.js';
-
-    File::shouldReceive('exists')
-        ->with($vitePath)
-        ->andReturnTrue();
-
-    File::shouldReceive('exists')
-        ->with($viteStubPath)
-        ->andReturnFalse();
-
-    PublishAssetsHandler::make($this->package, $this->author)();
-
-    expect(File::exists($vitePath))->toBeTrue();
-    expect(File::exists($viteStubPath))->toBeFalse();
-})->with([
-    fn () => new Package(
-        name: str()->studly('test-package'),
-        vendor: 'test-vendor',
-        filamentPlugin: FilamentPlugin::from([
-            'isStandalone' => false,
-            'pluginName' => 'Test',
-        ]),
-        asset: Asset::from([
-            'withCss' => true,
-            'withJs' => false,
-        ]),
-        withAssets: true,
-    ),
-]);
+it('should publish vite config', function () {
+    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/vite.config.js'))->toBeTrue();
+});
