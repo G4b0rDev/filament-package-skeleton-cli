250a038e964768afcb10a984b51cd4fabfd484d3
g4b0rdev <gabordesigns@gmail.com>
2024-12-22 21:30:58 +0100

    style: Fix lint

--- tests/Unit/PublishAssetsHandlerTest.php
+++ tests/Unit/PublishAssetsHandlerTest.php
@@ -9,12 +9,12 @@ use Illuminate\Support\Facades\File;
 beforeEach(function () {
     App::instance(
         ConfigHandler::class,
-        new ConfigHandler(configPath: __DIR__.'/../Package/filament-package-skeleton/config.json')
+        new ConfigHandler(configPath: __DIR__ . '/../Package/filament-package-skeleton/config.json')
     );
 
-    Config::set('path', __DIR__.'/../Package');
+    Config::set('path', __DIR__ . '/../Package');
 
-    Arr::map(File::directories(__DIR__.'/../Package'), fn (string $directory) => File::deleteDirectory($directory));
+    Arr::map(File::directories(__DIR__ . '/../Package'), fn (string $directory) => File::deleteDirectory($directory));
 
     $packages = [
         [
@@ -52,24 +52,24 @@ beforeEach(function () {
 });
 
 afterAll(function () {
-    File::deleteDirectory(__DIR__.'/../Package/my-simple-plugin');
+    File::deleteDirectory(__DIR__ . '/../Package/my-simple-plugin');
 });
 
 it('should publish tailwindcss config and base css', function () {
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/tailwindcss.config.js'))->toBeTrue();
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/css'))->toBeTrue();
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/css/app.css'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin/tailwindcss.config.js'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin/resources/css'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin/resources/css/app.css'))->toBeTrue();
 });
 
 it('should not publish tailwindcss config and base css if no assets are set', function () {
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin2/tailwindcss.config.js'))->toBeFalse();
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin2/resources/css'))->toBeFalse();
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin2/resources/css/app.css'))->toBeFalse();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin2/tailwindcss.config.js'))->toBeFalse();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin2/resources/css'))->toBeFalse();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin2/resources/css/app.css'))->toBeFalse();
 });
 
 it('should publish javascript skeleton', function () {
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/js'))->toBeTrue();
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/js/app.js'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin/resources/js'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin/resources/js/app.js'))->toBeTrue();
 });
 
 it('should set the default javascript file name as the package name if no custom name is set', function () {
@@ -92,13 +92,13 @@ it('should set the default javascript file name as the package name if no custom
 
     generatePackage($package, $author, $asset);
 
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin3/resources/js/my-simple-plugin3.js'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin3/resources/js/my-simple-plugin3.js'))->toBeTrue();
 });
 
 it('should publish the views skeleton', function () {
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/resources/views'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin/resources/views'))->toBeTrue();
 });
 
 it('should publish vite config', function () {
-    expect(file_exists(__DIR__.'/../Package/my-simple-plugin/vite.config.js'))->toBeTrue();
+    expect(file_exists(__DIR__ . '/../Package/my-simple-plugin/vite.config.js'))->toBeTrue();
 });
