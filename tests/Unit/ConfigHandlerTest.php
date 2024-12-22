7ac5c9bc6de44d20a3d975e508435f8c942830bf
g4b0rdev <gabordesigns@gmail.com>
2024-12-22 02:12:14 +0100

    chore: Refactor test case

--- tests/Unit/ConfigHandlerTest.php
+++ tests/Unit/ConfigHandlerTest.php
@@ -1,10 +1,15 @@
 <?php
 
 use App\ConfigHandler;
+use App\Facades\Config;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
 
 beforeEach(function () {
-    $this->config = app(ConfigHandler::class, ['configPath' => __DIR__.'/../Package/filament-package-skeleton/config.json']);
+    App::instance(
+        ConfigHandler::class,
+        new ConfigHandler(configPath: __DIR__.'/../Package/filament-package-skeleton/config.json')
+    );
 });
 
 afterAll(function () {
@@ -19,35 +24,34 @@ it('should create a config file if it does not exist', function () {
 });
 
 it('can get a config value', function () {
-    $this->config->set('path', '/home/username/projects');
-    expect($this->config->get('path'))->toBe('/home/username/projects');
+    Config::set('path', '/home/username/projects');
+
+    expect(Config::get('path'))->toBe('/home/username/projects');
 });
 
 it('should return a default value when the config value does not exist', function () {
-    $this->config->set('path', null);
-    expect($this->config->get('path', 'default'))->toBe('default');
+    Config::set('path', null);
+    expect(Config::get('path'))->toBe(null);
 });
 
 it('should throw an exception when trying to get a non-existent property', function () {
-    $this->config->get('nonExistentProperty');
+    Config::get('nonExistentProperty');
 })->throws(InvalidArgumentException::class);
 
 it('can set a config value', function () {
-    $this->config->set('path', '/home/username/projects');
-    $this->config->set('vendorName', 'test-author');
+    Config::set('path', '/home/username/projects');
+    Config::set('vendorName', 'test-author');
 
-    expect($this->config->get('path'))->not()->toThrow(InvalidArgumentException::class);
+    expect(Config::get('path'))->not()->toThrow(InvalidArgumentException::class);
 });
 
 it('should throw an exception when trying to set a non-existent property', function () {
-    $this->config->set('nonExistentProperty', 'value');
+    Config::set('nonExistentProperty', 'value');
 })->throws(InvalidArgumentException::class);
 
 it('can save a config value', function () {
-    $this->config->set('path', '/home/username/projects');
-    $this->config->save();
+    Config::set('path', '/home/username/projects');
+    Config::save();
 
-    $config = app(ConfigHandler::class, ['configPath' => __DIR__.'/../Package/filament-package-skeleton/config.json']);
-
-    expect($config->get('path'))->toBe('/home/username/projects');
+    expect(Config::get('path'))->toBe('/home/username/projects');
 });
