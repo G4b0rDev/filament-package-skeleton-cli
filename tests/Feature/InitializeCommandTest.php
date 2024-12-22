f33520c3dafc7aa5b02857f3098c68cbd8f4b456
g4b0rdev <gabordesigns@gmail.com>
2024-12-22 02:06:52 +0100

    chore: Update test cases with datasets

--- tests/Feature/InitializeCommandTest.php
+++ tests/Feature/InitializeCommandTest.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\ConfigHandler;
+use App\DataTransferObjects\Asset;
+use App\DataTransferObjects\Author;
+use App\DataTransferObjects\Package;
 use App\Exceptions\ProjectAlreadyExistsException;
 use App\Facades\Config;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Process;
@@ -17,53 +21,64 @@ beforeEach(function () {
 });
 
 afterAll(function () {
-    File::deleteDirectory(__DIR__.'/../Package/test-package');
-    File::deleteDirectory(__DIR__.'/../Package/test-package2');
+    Arr::map(File::directories(__DIR__.'/../Package'), fn (string $directory) => File::deleteDirectory($directory));
 });
 
-it('should initialize a new package project without assets', function () {
+it('should initialize a new package project without assets', function (array $package, array $author) {
     Process::partialMock()->shouldReceive('concurrently')
         ->andReturn(0);
 
+    $package = Package::from($package);
+    $author = Author::from($author);
+
     $this->artisan('new')
-        ->expectsQuestion('Author name', 'Test User')
-        ->expectsQuestion('Author email', 'test@example.com')
-        ->expectsQuestion('Package name', 'test-package')
-        ->expectsQuestion('Vendor name', 'test-user')
-        ->expectsConfirmation('Do you want to create a standalone filament package?', 'no')
-        ->expectsConfirmation('Do you want have custom assets?', 'no')
+        ->expectsQuestion('Author name', $author->name)
+        ->expectsQuestion('Author email', $author->email)
+        ->expectsQuestion('Package name', $package->name)
+        ->expectsQuestion('Vendor name', $package->vendor)
+        ->expectsConfirmation('Do you want to create a standalone filament package?', $package->filamentPlugin->isStandalone)
+        ->expectsConfirmation('Do you want have custom assets?', $package->withAssets)
         ->assertExitCode(0);
-});
+})->with('defaultPackage');
 
-it('should throw an expection if project directory already exists', function () {
+it('should throw an expection if project directory already exists', function (array $package, array $author) {
     Process::partialMock()->shouldReceive('concurrently')
         ->andReturn(0);
 
+    $package = Package::from($package);
+    $author = Author::from($author);
+
     $this->artisan('new')
-        ->expectsQuestion('Author name', 'Test User')
-        ->expectsQuestion('Author email', 'test@example.com')
-        ->expectsQuestion('Package name', 'test-package')
-        ->expectsQuestion('Vendor name', 'test-user')
-        ->expectsConfirmation('Do you want to create a standalone filament package?', 'no')
-        ->expectsConfirmation('Do you want have custom assets?', 'no')
+        ->expectsQuestion('Author name', $author->name)
+        ->expectsQuestion('Author email', $author->email)
+        ->expectsQuestion('Package name', $package->name)
+        ->expectsQuestion('Vendor name', $package->vendor)
+        ->expectsConfirmation('Do you want to create a standalone filament package?', $package->filamentPlugin->isStandalone)
+        ->expectsConfirmation('Do you want have custom assets?', $package->withAssets)
         ->assertExitCode(0);
-})->throws(ProjectAlreadyExistsException::class);
+})
+    ->with('defaultPackage')
+    ->throws(ProjectAlreadyExistsException::class);
 
-it('should initialize a new package project', function () {
+it('should initialize a new package project', function (array $package, array $author, array $asset) {
     Process::partialMock()->shouldReceive('concurrently')
         ->andReturn(0);
 
+    $asset = Asset::from($asset);
+    $package = Package::from([...$package, 'assets' => $asset]);
+    $author = Author::from($author);
+
     $this->artisan('new')
-        ->expectsQuestion('Author name', 'Test User')
-        ->expectsQuestion('Author email', 'test@example.com')
-        ->expectsQuestion('Package name', 'test-package2')
-        ->expectsQuestion('Vendor name', 'test-user')
-        ->expectsConfirmation('Do you want to create a standalone filament package?', 'no')
+        ->expectsQuestion('Author name', $author->name)
+        ->expectsQuestion('Author email', $author->email)
+        ->expectsQuestion('Package name', $package->name.'2')
+        ->expectsQuestion('Vendor name', $package->vendor)
+        ->expectsConfirmation('Do you want to create a standalone filament package?', $package->filamentPlugin->isStandalone)
         ->expectsConfirmation('Do you want have custom assets?', 'yes')
         ->expectsConfirmation('Do you need css with TailwindCSS setup?', 'yes')
-        ->expectsQuestion('Do you add a custom css file name?', 'app')
+        ->expectsQuestion('Do you add a custom css file name?', $package->asset->cssName)
         ->expectsConfirmation('Do you need js with AlpineJS setup?', 'yes')
-        ->expectsQuestion('Do you add a custom js file name?', 'app')
+        ->expectsQuestion('Do you add a custom js file name?', $package->asset->jsName)
         ->expectsConfirmation('Do you need blade templates/views?', 'yes')
         ->assertExitCode(0);
-});
+})->with('packageWithAsset');
