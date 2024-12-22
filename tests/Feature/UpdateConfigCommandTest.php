5e2ab4609e1124661d0001b6d552cc94c783682f
g4b0rdev <gabordesigns@gmail.com>
2024-12-22 21:40:21 +0100

    test: Enhance configuration update tests with success case

--- tests/Feature/UpdateConfigCommandTest.php
+++ tests/Feature/UpdateConfigCommandTest.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\ConfigHandler;
+use App\Facades\Config;
+use Illuminate\Support\Facades\App;
 use LaravelZero\Framework\Commands\Command;
+
+beforeEach(function () {
+    App::instance(
+        ConfigHandler::class,
+        new ConfigHandler(configPath: __DIR__.'/../Package/filament-package-skeleton/config.json')
+    );
+});
 
 it('should cancel the configuration update', function () {
     $this->artisan('config')
@@ -15,6 +25,17 @@ it('should abort the configuration update before change accepted', function () {
         ->expectsQuestion('Relative root path', '/tmp/projects')
         ->expectsQuestion('Vendor name', 'acme')
         ->expectsConfirmation('Do you want to update the configuration?', 'no')
-        ->expectsOutputToContain('Aborted.')
         ->assertExitCode(Command::FAILURE);
+});
+
+it('should update the configuration', function () {
+    $this->artisan('config')
+        ->expectsConfirmation('Do you want to update the configuration?', 'yes')
+        ->expectsQuestion('Relative root path', '/tmp/projects')
+        ->expectsQuestion('Vendor name', 'acme')
+        ->expectsConfirmation('Do you want to update the configuration?', 'yes')
+        ->assertExitCode(Command::SUCCESS);
+
+    expect(Config::get('path'))->toBe('/tmp/projects');
+    expect(Config::get('vendorName'))->toBe('acme');
 });
