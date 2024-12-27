b63a570cc754a3d0cb661b94cef8a842407809f8
g4b0rdev <gabordesigns@gmail.com>
2024-12-27 21:20:23 +0100

    chore: Update repository cloning to support access tokens

--- app/Actions/PublishProjectAction.php
+++ app/Actions/PublishProjectAction.php
@@ -25,22 +25,21 @@ class PublishProjectAction
         File::ensureDirectoryExists($path);
 
         Log::debug("Cloning repository to {$path}");
-        $process = Process::run("git clone --depth 1 git@github.com:G4b0rDev/filament-package-skeleton.git {$path}");
-        
+
+        $token = env('ACCESS_TOKEN');
+        $cloneCommand = $token
+            ? "git clone --depth 1 https://{$token}:x-oauth-basic@github.com/G4b0rDev/filament-package-skeleton.git {$path}"
+            : "git clone --depth 1 git@github.com:G4b0rDev/filament-package-skeleton.git {$path}";
+
+        $process = Process::run($cloneCommand);
+
         if ($process->exitCode() !== 0) {
-            throw new RuntimeException("Failed to clone repository: " . $process->errorOutput());
+            throw new RuntimeException('Failed to clone repository: '.$process->errorOutput());
         }
 
         Log::debug('Cloned filament-package-skeleton');
 
-        // Check the contents of the cloned directory
-        $contents = File::allFiles($path);
-        foreach ($contents as $file) {
-            Log::debug("Cloned file: {$file->getPathname()}");
-        }
-
         Process::path($path)->run("rm -rf {$path}/.git");
-
         Process::path($path)->run("cd {$path} && git init && git branch -M main");
     }
 }
